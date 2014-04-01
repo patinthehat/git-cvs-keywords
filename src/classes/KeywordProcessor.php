384ad9c2db65160060dc7e841c2a136a551701bb
trick <trick.developer@gmail.com>
2014-04-01 03:31:32 -0400

    * src/KeywordProcessor.php  - Lots of code cleanup  - KeywordChecking is now done with a regex generated at runtime, to make adding new Keywords easier.  - added define PROCESSOR_CHECK_FOR_KEYWORDS_RE that holds the regex to check for keywords.  - commented out regular expression replacement timing, but can be reenabled to check regex performance

--- src/classes/KeywordProcessor.php
+++ src/classes/KeywordProcessor.php
@@ -9,13 +9,31 @@
 require_once('src/classes/ApplicationData.php');
 require_once('src/classes/BasicTextProcessor.php');
 
+if (!defined('PROCESSOR_CHECK_FOR_KEYWORDS_RE')) {
+  $ValidKeywords = array('Author', 'Date', 'Header', 'Id', 'Revision', 'Tags');
+  $CheckForValidKeywordsRegEx = KeywordProcessor::GenerateRegExKeywordMatching($ValidKeywords);
+  define('PROCESSOR_CHECK_FOR_KEYWORDS_RE', $CheckForValidKeywordsRegEx);
+}
 
-class KeywordProcessor extends \BasicTextProcessor {
+class KeywordProcessor extends BasicTextProcessor {
 
+  
+  /**
+   * Generates a regular expression that checks a file for valid Keywords.
+   * @param array $Keywords
+   * @return string
+   */
+  public static function GenerateRegExKeywordMatching(array $Keywords) {
+    //'/\$(Author|Date|Header|Id|Revision|Tags)(\: |\$)/'
+    $kws = implode('|', $Keywords);
+    return sprintf('/\$(%s)(\: |\$)/', $kws);
+  }
+  
+  
   //TODO change data storage - not happy with the way data is passed around
   public static function checkKeywords($text) {
     global $appData;
-    $hasKeywords = '/\$(Author|Date|Header|Id|Revision|Tags)(:|\$)/';
+    $hasKeywords =PROCESSOR_CHECK_FOR_KEYWORDS_RE;
     if (preg_match($hasKeywords, $text, $m) > 0) {
       $appData->haskeywords = true;
     } else {
@@ -24,52 +42,38 @@ class KeywordProcessor extends \BasicTextProcessor {
     return $appData->haskeywords;
   }
   
-  public static function processText($text) {
+  static function processText($text) {
     global $appData;
     //process text and return it
     $kwReplaceCount = 0;
     
-    $hasKeywords = '/\$(Author|Date|Header|Id|Revision|Tags)(\: |\$)/';
+    $hasKeywords = PROCESSOR_CHECK_FOR_KEYWORDS_RE;
     if (preg_match($hasKeywords, $text, $m) > 0) {
       $appData->haskeywords = true;
+      
       if (strpos($text,"@@IGNORE"."KEYWORDS")!==false) {
         return $text; //no processing
       }
       //found valid keyword string in this file, so process it
 
       $patterns = array(
-         // '/(@@([SE]IGNORE))/',
-          '/\$(Author|Date|Header|Tags|Revision)(\$|: [^\\$]* \$)/',
-          //'/\$(Date)(\$|: [^$]* \$)/',
-         // '/\$(Header)(\$|: [^$]* \$)/',
-          "/\\$(Id)(\\$|: ".str_replace('/','\/', $appData->Filename)." [^$]* \\$)/",          
-         // '/\$(Revision)(\$|: [^$]* \$)/',
-         // '/\$(Tags)(\$|: [^$]* \$)/',
+          '/\$(Author|Date|Header|Tags|Revision)(\$|: [^\\$]* \$)/',  //match most keywords 
+          "/\\$(Id)(\\$|: ".str_replace('/','\/', $appData->Filename)." [^$]* \\$)/",   /// match (Id filename  ... ) keyword seperately
       );
+      //generate Id data
       $IdData = $appData->Filename ." ". $appData->Revision ." " . $appData->Date ." ". $appData->Author ." ".rand(1000,9999)." ".$appData->Tags;
       $appData->Id = $IdData;
-      /*
-      $replacements = array(
-         // "___IGNORE_KEYWORDS $2 $1  IGNORE_KEYWORDS__",
-          '\$$1: '.$appData->Author.' \$',
-          '\$Date: '.$appData->Date  .' \$',
-          '\$Header: '.$appData->Header  .' \$',
-          '\$Id: '.$IdData  .' \$',
-          '\$Revision: '.$appData->Revision  .' \$',
-          '\$$1: '.$appData->Tags  .' \$',  
-      );*/
       
-     
-      
-      //print_r($patterns);
       
       $start = microtime();
-      $text = preg_replace_callback($patterns, function($m) { 
-          global $appData;  //print_r($m); 
-           return "$".$m[1].": ".$appData->{$m[1]}." $";  }, $text, -1, $kwReplaceCount);
-      
-        $end = microtime();
-        printf("$kwReplaceCount %.4f\n", $end*1000 - $start*1000);
+      $text = preg_replace_callback($patterns, 
+        function($m) { 
+            global $appData; 
+             return "$".$m[1].": ".$appData->{$m[1]}." $";  
+        }, $text, -1, $kwReplaceCount);
+
+      $end = microtime();
+      //printf("$kwReplaceCount %.4f\n", $end*1000 - $start*1000);
     } else {
       //no keywords found, do nothing
       $appData->haskeywords = false;
